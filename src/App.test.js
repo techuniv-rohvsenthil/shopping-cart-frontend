2e07da78ef91a8d211436496656ace8b6f2f4064
Rohini Senthil <rohinivsenthil@gmail.com>
2020-03-05 12:31:41 +0530

    fix: update testcase describe statement

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-describe('the ViewGeneratedTemplates component', () => {
+describe('the App component', () => {
   it('should render correctly', () => {
     const { asFragment } = render(<App />);
     expect(asFragment()).toMatchSnapshot();
--- src/components/Header/index.test.jsx
+++ src/components/Header/index.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Header from './index';
 
-describe('the ViewGeneratedTemplates component', () => {
+describe('the Header component', () => {
   it('should render correctly', () => {
     const { asFragment } = render(<Header />);
     expect(asFragment()).toMatchSnapshot();
