f2121360524b6c32ba43e2c6cb588ae280fd05e6
Rohini Senthil <rohinivsenthil@gmail.com>
2020-03-06 09:50:02 +0530

    fix[css]: made cart responsive

--- src/containers/Cart/index.jsx
+++ src/containers/Cart/index.jsx
@@ -6,37 +6,37 @@ import Table from '../../components/Table';
 import useCartDetails from '../../hooks/useCartDetails';
 
 const Cart = () => {
-    const [data] = useCartDetails();
-    let items = {};
-    data.forEach((item) => {
-      const count = (!items[item.item])? 1: items[item.item].quantity + 1;
-      return(
-        items[item.item] = {
+  const [data] = useCartDetails();
+  let items = {};
+  data.forEach((item) => {
+    const count = (!items[item.item]) ? 1 : items[item.item].quantity + 1;
+    return (
+      items[item.item] = {
         name: item.item,
         price: item.price,
         quantity: count,
       }
-      )
-    })
+    )
+  })
   return (
     <div>
       <div className={styles.positioning}>
-  <div className={styles.cart}>{data.length}</div>
+        <div className={styles.cart}>{data.length}</div>
       </div>
       <Header />
       <div className={styles.title}>
-          Your Shopping Cart ({data.length} items)
+        Your Shopping Cart ({data.length} items)
       </div>
       <div className={styles.components}>
-          <div>
-            <Table items={items}/>
+        <div>
+          <Table items={items} />
+        </div>
+        <div data-testid="container-div">
+          <div data-testid="checkout-div">
+            <Checkout cart={data} />
           </div>
-          <div data-testid="container-div">
-              <div  data-testid="checkout-div">
-            <Checkout cart={data}/>
-            </div>
-            <button type="button" className={styles.button} onClick={() => {window.history.back()}}>Continue Shopping</button>
-          </div>
+          <button type="button" className={styles.button} onClick={() => { window.history.back() }}>Continue Shopping</button>
+        </div>
       </div>
     </div>
   );
